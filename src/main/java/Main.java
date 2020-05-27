2142f3f9597a7cf2a656200ec25209579448d58f
dezoxyr <aclement@et.esiea.fr>
2020-05-27 15:19:32 +0200

    Version commentée

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,18 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Debut");
 		Client c1 = new Client();
 		Client c2 = new Client();
 		Serveur s = new Serveur();
-		s.connect("127.0.0.1", 6666);
-		s.open();
-		c1.connect("127.0.0.1",6666);;
-		c2.connect("127.0.0.1",6666);;
+		
+		s.connect("127.0.0.1", 6666);//Crée un serveur a l'ip "127.0.0.1" et sur le port 6666
+		s.open();					//Ouvre le serveur
+		
+		c1.connect("127.0.0.1",6666);//Connecte un client sur l'ip "127.0.0.1" et sur le port 6666 renvoi une erreur si il n'y a pas de serveur ouvert
+		c2.connect("127.0.0.1",6666);
 		try {
-			Thread.sleep(300);
-			c1.msg("Bonjour je suis Malko");
+			Thread.sleep(300);	//tempo afin de laisser le temps de connecter les clients au serveur
+			c1.msg("Bonjour je suis Malko");//envoie un message au serveur
 			
 			c2.msg("Bonjour je suis Alexis");
 			
